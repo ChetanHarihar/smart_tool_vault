3e70a3ff6a28cd4210183261f42b979c4014daee
Chetan Harihar <chetanhs11@gmail.com>
2024-06-05 21:16:07 +0530

    modified main.py msgbox import

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from settings.config import *
-from services import msgbox
+from gui_components.widgets import msgbox
 import os
 
 if __name__ == "__main__":
