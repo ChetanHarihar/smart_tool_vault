7577088b8080e3407630541374682bdc1efa144f
Chetan <chetanhs11@gmail.com>
2024-06-16 13:04:09 +0530

    created an instance of login panel in main.py

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import tkinter as tk
 from tkinter import ttk
 from settings.config import *
 from gui_components.widgets import msgbox
+from login_panel import LoginPanel
 import os
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.geometry(SCREEN_SIZE)
+    root.title("Smart Tool Vault")
     style = ttk.Style(root)
 
     # build the path to the theme file
@@ -17,5 +19,9 @@ if __name__ == "__main__":
 
     # Bind the on_closing function to the window close event
     root.protocol("WM_DELETE_WINDOW", lambda:msgbox.on_exit(root))
+
+    # Create an instance of LoginPanel and pack it into the root window
+    app = LoginPanel(root)
+    app.pack()
     
     root.mainloop()
