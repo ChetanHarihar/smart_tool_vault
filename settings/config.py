52fcb0147f8bd709af7cd188e977666c06c9e4ef
Chetan <chetanhs11@gmail.com>
2024-06-17 16:04:56 +0530

    added email functionality variables

--- settings/config.py
+++ settings/config.py
@@ -4,3 +4,8 @@ THEME_PATH = "/home/pi/Python/smart_tool_vault"
 # MQTT Broker details
 MQTT_SERVER = '192.168.206.10'
 MQTT_PORT = 1883
+
+MIN_STOCK = {'DRILL': 20, 'TAP': 10, 'THREAD ROLLER': 10, 'INSERT': 10, 'REAMER': 10, 'HOLDER': 10,}
+EMAIL_SENDER = 'prathapj.ei20@bmsce.ac.in'
+EMAIL_PASSWORD = 'njipmbrcobpjwcim' 
+EMAIL_RECEIVER = 'kushalm518@gmail.com'
